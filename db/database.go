cb8b431e5dac5b629a4143d3a21bdf59679a541a
naveed007 <naveedakhtar019@gmail.com>
2020-09-16 09:01:56 -0700

    update 1

---
+++ db/database.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
+
+func CreateDatabase() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/naveed007/restApiDemo1/app"
+	"github.com/naveed007/restApiDemo1/db"
+)
+
+func main() {
+	database, err := db.CreateDatabase()
+	if err != nil {
+		log.Fatal("Database connection failed: %s", err.Error())
+	}
+
+	app := &app.App{
+		Router:   mux.NewRouter().StrictSlash(true),
+		Database: database,
+	}
+
+	app.SetupRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", app.Router))
+}
